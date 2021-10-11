84788ac17bf75ffd7a4de313cfbbd70a41a95ced
Someone Rude <example@mail.com>
2021-10-11 18:41:50 +0300

    Remove part of files

--- ex7.py
+++
@@ -1,8 +0,0 @@
-import turtle
-
-for i in range(100):
-	turtle.left(30)
-	turtle.forward(i / 5)
-	
-turtle.done()
-
--- ex8.py
+++
@@ -1,8 +0,0 @@
-import turtle
-
-for i in range(1, 200, 5):
-	turtle.left(90)
-	turtle.forward(i)
-	
-turtle.done()
-
